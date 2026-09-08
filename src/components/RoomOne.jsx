ea198799a0383684466f485be68bf0569ad85718
pheephiphophum <finnsnipes99@gmail.com>
2026-09-08 15:00:50 -0600

    finished css layout for now

--- src/components/RoomOne.jsx
+++ src/components/RoomOne.jsx
@@ -10,13 +10,12 @@ function Camp(){
         <>
             <img src={Background} className="bgImg"/>
             <div className='gameBorder'>
+            <div className='statBox'>
 
             </div>
             <div className='interactBox'>
 
             </div>
-            <div className='statBox'>
-
             </div>
             <div className='buttonBox'>
                 <button>Inventory</button>
