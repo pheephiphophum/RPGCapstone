108bea355ecdb1ccb68f89a2ff274501482b2ebf
PureVessle <stillerec@gmail.com>
2026-09-08 10:39:06 -0600

    made home display based on screen dimentions

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,17 +8,19 @@ function Home(){
     return(
         <>
             <div className='homeScreen'>
-                <h1>They're Coming...</h1>
-                <h2>Can You Survive?</h2>
-                <p>An apocolypse survival, puzzle RPG Roguelike</p>
+                <div className='blackBox'>
+                    <h1>They're Coming...</h1>
+                    <h2>Can You Survive?</h2>
+                    <p>An apocolypse survival, puzzle RPG Roguelike</p>
+                    <Link to='/roomone'>
+                        <button className='button'>START</button>
+                    </Link>
+                </div>
+                
                 <img src={Fart} className="bgImg"/>
-                <Link to='/roomone'>
-                    <button className='button'>START</button>
-                </Link>
+                
             </div>
-            <div className='blackBox'>
-                <p>don't mind me I'm just a box back here teehee nothing to see</p>
-            </div>
+            
         </>
     )
 }
