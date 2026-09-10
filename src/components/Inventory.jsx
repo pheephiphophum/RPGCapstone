a66571630219258e6fc7b79eb8258195484a869a
PureVessle <stillerec@gmail.com>
2026-09-10 10:55:51 -0600

    most of the inventory system

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -1,19 +1,27 @@
+import { useState, useEffect } from 'react'
+
 import '../styles/inventory.css'
-import '../styles/roomOne.css'
 
 
-const itemsTemp = ['sword', 'chestplate', 'booties', 'golden skull of rauhl', 'bagel']
+const itemsTemp = ['sword', 'chestplate', 'booties', 'golden skull of rauhl', 'bagel', '1', '2', '3', '4', '5', '6', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', '7', 'really really long name that doesnt fit inside the box so i need to figure out what to do with it']
 
 function Inventory(){
+
+    console.log(itemsTemp)
     return(
         <>
-            <div id="" className="interactBox">
-                <ul>
-                    {itemsTemp.map((itemsTemp) => ( 
-                        <div key={itemsTemp} className='item'>{itemsTemp}</div>
-                    ))}
-                </ul>
+            <div className='inventoryOverlay' > 
+                {itemsTemp.map((itemsTemp) => ( 
+                    <div key={itemsTemp} className='item'>{itemsTemp}</div>
+                ))}
             </div>
         </>
     )
 }
+
+// function InventoryToggle(){
+//     if()
+// }
+
+
+export default Inventory
--- src/components/RoomOne.jsx
+++ src/components/RoomOne.jsx
@@ -3,6 +3,11 @@ import { Link, useNavigate } from 'react-router-dom'
 import ProfileIcon from '../assets/download20260905131048.png'
 import Background from '../assets/zombies-hands.png'
 import '../styles/roomOne.css'
+import { useEffect, useState } from 'react'
+import Inventory from '../components/Inventory'
+
+
+
 
 function Camp(){
 
@@ -12,6 +17,7 @@ function Camp(){
             <div className='gameContainer'>
                 <div className='subContainer'>
                     <div className='interactBox'>
+                        <Inventory/>
 
                     </div>
 
@@ -21,7 +27,7 @@ function Camp(){
 
                 </div>
                 <div className='buttonBox'>
-                    <button>Inventory</button>
+                    <button >Inventory</button>
                     <button>Search</button>
                     <button>Loot</button>
                     <button>Map</button>
